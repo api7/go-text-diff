e742fc697543b4a3f055d0abcfb26be042fb105d
JunXu Chen <chenjunxu6@gmail.com>
2022-06-01 23:37:15 +0800

    feat: support diff by files (#3)

--- diff.go
+++ diff.go
@@ -1,13 +1,37 @@
 package gotextdiff
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/api7/gotextdiff/diff"
 	"github.com/api7/gotextdiff/myers"
 	"github.com/api7/gotextdiff/span"
 )
 
-func StringsDiff(str1, str2 string, options ...diff.Option) diff.Unified {
-	edits := myers.ComputeEdits(span.URIFromPath("t"), str1, str2)
+func StringsDiff(str1, str2 string, options ...diff.Option) *diff.Unified {
+	edits := myers.ComputeEdits(span.URIFromPath(""), str1, str2)
+	unified := diff.ToUnified("", "", str1, edits, options...)
 
-	return diff.ToUnified("", "", str1, edits, options...)
+	return &unified
+}
+
+func FilesDiff(fileFrom, fileTo string, options ...diff.Option) *diff.Unified {
+	content1, err := ioutil.ReadFile(fileFrom)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read file %s: %s", fileFrom, err)
+		os.Exit(1)
+		return nil
+	}
+	content2, err := ioutil.ReadFile(fileTo)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to read file %s: %s", fileTo, err)
+		os.Exit(1)
+		return nil
+	}
+
+	edits := myers.ComputeEdits(span.URIFromPath(fileFrom), string(content1), string(content2))
+	unified := diff.ToUnified(fileFrom, fileTo, string(content1), edits, options...)
+	return &unified
 }
--- example/example.go
+++ example/example.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/api7/gotextdiff"
 
+	"github.com/api7/gotextdiff"
 	"github.com/api7/gotextdiff/diff"
 )
 
 func main() {
+
 	aString := `The Way that can be told of is not the eternal Way;
 The name that can be named is not the eternal name.
 The Nameless is the origin of Heaven and Earth;
@@ -34,4 +35,10 @@ But after they are produced,
 
 	fmt.Print(gotextdiff.StringsDiff(aString, bString,
 		diff.Colorful(true), diff.OmitEOL(true)))
+
+	fmt.Println("")
+
+	fmt.Print(gotextdiff.FilesDiff("./example/fixtures/config.yaml", "./example/fixtures/config2.yaml",
+		diff.Colorful(true), diff.OmitEOL(true)))
+
 }
